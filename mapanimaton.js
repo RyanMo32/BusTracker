c127f4ca9ca62747054895863d4ff4d944e2c0a1
Ryan <90024971+RyanMo32@users.noreply.github.com>
2021-12-13 21:17:45 -0800

    Update mapanimaton.js

--- mapanimaton.js
+++ mapanimaton.js
@@ -17,7 +17,7 @@ const busStops = [
 mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1vMzIiLCJhIjoiY2t3Z3J6MmNiMDZsbzJvbDVxb2JkendnYiJ9.NcCYs2AmQwTdkPCaXdFjSA';
 
 // This is the map instance
-let map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [-71.104081, 42.365554],
